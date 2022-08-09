5489f12894ab3fa95b70ca4da4b24948dfaa658c
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-08-09 22:48:04 +0200

    change encodings

--- src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
+++ src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
@@ -1,6 +1,7 @@
 package info.unterrainer.commons.overmindjsonata4javaserver;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
@@ -43,8 +44,8 @@ public class Server {
 				ZMsg msg = ZMsg.recvMsg(socket);
 				log.info("New message received.");
 				if (msg != null) {
-					String query = msg.popString();
-					String json = msg.popString();
+					String query = new String(msg.pop().getData(), StandardCharsets.UTF_8);
+					String json = new String(msg.pop().getData(), StandardCharsets.UTF_8);
 					msg.destroy();
 					log.info("Received query [{}]", query);
 					log.debug("Received json [{}]", json);
@@ -53,7 +54,7 @@ public class Server {
 
 					log.info("Sending result [{}].", result);
 					ZMsg reply = new ZMsg();
-					reply.add(result);
+					reply.add(result.getBytes(StandardCharsets.UTF_8));
 					reply.send(socket);
 					reply.destroy();
 				} else
