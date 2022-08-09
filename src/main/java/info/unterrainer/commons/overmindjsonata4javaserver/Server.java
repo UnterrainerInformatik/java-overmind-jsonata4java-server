4d07e27630e642eddf0266e27211e0d4f386ee21
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-08-09 15:44:24 +0200

    add jsonata and logic

--- src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
+++ src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
@@ -1,9 +1,17 @@
 package info.unterrainer.commons.overmindjsonata4javaserver;
 
+import java.io.IOException;
+
 import org.zeromq.SocketType;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 import org.zeromq.ZMsg;
+
+import com.api.jsonata4java.expressions.EvaluateException;
+import com.api.jsonata4java.expressions.Expressions;
+import com.api.jsonata4java.expressions.ParseException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,7 +20,18 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Server {
 
+	ObjectMapper mapper = new ObjectMapper();
+
 	public static void main(final String[] args) throws Exception {
+		try {
+			new Server().run();
+		} catch (Exception e) {
+			log.error("uncaught exception", e);
+		}
+	}
+
+	public void run() throws IOException {
+
 		try (ZContext context = new ZContext()) {
 			// Socket to talk to clients
 			ZMQ.Socket socket = context.createSocket(SocketType.REP);
@@ -22,15 +41,15 @@ public class Server {
 				// Block until a message is received
 				ZMsg msg = ZMsg.recvMsg(socket);
 				if (msg != null) {
-					String value = readAll(msg);
+					String query = msg.popString();
+					String json = msg.popString();
 					msg.destroy();
 
-					System.out.println("Received: [" + value + "]");
+					String result = jsonataQuery(query, json);
+
 					// Send a response
 					ZMsg reply = new ZMsg();
-					reply.add("Hello. Your message was: \"");
-					reply.add(value);
-					reply.add("\"");
+					reply.add(result);
 					reply.send(socket);
 					reply.destroy();
 				}
@@ -38,12 +57,37 @@ public class Server {
 		}
 	}
 
-	public static String readAll(final ZMsg msg) {
-		if (msg == null)
+	private String jsonataQuery(final String query, final String json) throws IOException {
+		if (query == null) {
+			log.error("Config-string of check was null.");
 			return null;
-		StringBuilder sb = new StringBuilder();
-		while (msg.peek() != null)
-			sb.append(msg.popString());
-		return sb.toString();
+		}
+
+		if (json == null) {
+			log.error("Json with data to query was null.");
+			return null;
+		}
+
+		JsonNode jsonObj = null;
+		try {
+			jsonObj = mapper.readTree(json);
+		} catch (IOException e) {
+			log.error("Error reading state-array-json.", e);
+		}
+
+		String result = null;
+		try {
+			Expressions expr = Expressions.parse(query);
+			JsonNode jsonResult = expr.evaluate(jsonObj);
+			final Object obj = mapper.treeToValue(jsonResult, Object.class);
+			result = mapper.writeValueAsString(obj);
+		} catch (ParseException e) {
+			log.error("Error parsing expression-json.", e);
+			throw new IOException(e);
+		} catch (EvaluateException e) {
+			log.error("Error evaluating expression-json.", e);
+			throw new IOException(e);
+		}
+		return result;
 	}
 }
