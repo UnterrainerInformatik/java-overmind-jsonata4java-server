be785de0748bae17ebe763a2ea2b51ddf66e95e8
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-08-09 23:13:02 +0200

    fix loop exception

--- src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
+++ src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
@@ -39,27 +39,30 @@ public class Server {
 			ZMQ.Socket socket = context.createSocket(SocketType.REP);
 			socket.bind("tcp://*:5555");
 
-			while (!Thread.currentThread().isInterrupted()) {
-				// Block until a message is received
-				ZMsg msg = ZMsg.recvMsg(socket);
-				log.info("New message received.");
-				if (msg != null) {
-					String query = new String(msg.pop().getData(), StandardCharsets.UTF_8);
-					String json = new String(msg.pop().getData(), StandardCharsets.UTF_8);
-					msg.destroy();
-					log.info("Received query [{}]", query);
-					log.debug("Received json [{}]", json);
+			while (!Thread.currentThread().isInterrupted())
+				try {
+					// Block until a message is received
+					ZMsg msg = ZMsg.recvMsg(socket);
+					log.info("New message received.");
+					if (msg != null) {
+						String query = new String(msg.pop().getData(), StandardCharsets.UTF_8);
+						String json = new String(msg.pop().getData(), StandardCharsets.UTF_8);
+						msg.destroy();
+						log.info("Received query [{}]", query);
+						log.debug("Received json [{}]", json);
 
-					String result = jsonataQuery(query, json);
+						String result = jsonataQuery(query, json);
 
-					log.info("Sending result [{}].", result);
-					ZMsg reply = new ZMsg();
-					reply.add(result.getBytes(StandardCharsets.UTF_8));
-					reply.send(socket);
-					reply.destroy();
-				} else
-					log.warn("Message was null.");
-			}
+						log.info("Sending result [{}].", result);
+						ZMsg reply = new ZMsg();
+						reply.add(result == null ? null : result.getBytes(StandardCharsets.UTF_8));
+						reply.send(socket);
+						reply.destroy();
+					} else
+						log.warn("Message was null.");
+				} catch (Exception e) {
+					log.error("uncaught exception", e);
+				}
 		}
 	}
 
