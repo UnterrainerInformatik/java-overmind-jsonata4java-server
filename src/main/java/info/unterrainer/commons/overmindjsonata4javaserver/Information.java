f6b8d9790efdcdde91e93b4c979b3ab34da01811
Gerald Unterrainer <gu_falcon@hotmail.com>
2022-08-09 15:55:28 +0200

    repair

---
+++ src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Information.java
@@ -0,0 +1,7 @@
+package info.unterrainer.commons.overmindjsonata4javaserver;
+
+public class Information {
+	public static final String name = "Overmind-Jsonatat4Java-Server";
+	public static final String buildTime = "2022-08-09T13:55:05Z";
+	public static final String pomVersion = "0.0.2";
+}
