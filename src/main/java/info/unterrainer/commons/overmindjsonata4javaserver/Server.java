9d6c1af61bb5944f9267bacba65722fdfa489ed0
psilo <gu_falcon@hotmail.com>
2022-11-22 17:59:57 +0100

    add basic gRPC

--- src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
+++ src/main/java/info/unterrainer/commons/overmindjsonata4javaserver/Server.java
@@ -1,12 +1,6 @@
 package info.unterrainer.commons.overmindjsonata4javaserver;
 
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
-import org.zeromq.SocketType;
-import org.zeromq.ZContext;
-import org.zeromq.ZMQ;
-import org.zeromq.ZMsg;
 
 import com.api.jsonata4java.expressions.EvaluateException;
 import com.api.jsonata4java.expressions.Expressions;
@@ -15,6 +9,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import info.unterrainer.commons.overmindjsonata4javaserver.implementations.GRpcServer;
+import info.unterrainer.commons.overmindjsonata4javaserver.implementations.SocketServer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,52 +18,21 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Server {
 
-	ObjectMapper mapper = new ObjectMapper();
+	static ObjectMapper mapper = new ObjectMapper();
 
 	public static void main(final String[] args) throws Exception {
 		try {
-			new Server().run();
+			GRpcServer gRpcServer = new GRpcServer();
+			gRpcServer.run();
+			new SocketServer().run();
+			
 		} catch (Exception e) {
 			log.error("uncaught exception", e);
 		}
 	}
 
-	public void run() throws IOException {
-
-		try (ZContext context = new ZContext()) {
-			// Socket to talk to clients
-			ZMQ.Socket socket = context.createSocket(SocketType.REP);
-			socket.bind("tcp://*:5555");
-
-			while (!Thread.currentThread().isInterrupted())
-				try {
-					// Block until a message is received
-					ZMsg msg = ZMsg.recvMsg(socket);
-					log.info("New message received.");
-					if (msg != null) {
-						String query = new String(msg.pop().getData(), StandardCharsets.UTF_8);
-						String json = new String(msg.pop().getData(), StandardCharsets.UTF_8);
-						msg.destroy();
-						log.info("Received query [{}]", query);
-						log.debug("Received json [{}]", json);
-
-						String result = jsonataQuery(query, json);
-
-						log.info("Sending result [{}].", result);
-						ZMsg reply = new ZMsg();
-						reply.add(result == null ? null : result.getBytes(StandardCharsets.UTF_8));
-						reply.send(socket);
-						reply.destroy();
-					} else
-						log.warn("Message was null.");
-				} catch (Exception e) {
-					log.error("uncaught exception", e);
-				}
-		}
-	}
-
 	// Gets a query and a json-array to query against and returns 'true' or not.
-	private String jsonataQuery(final String query, final String json) {
+	public static synchronized String jsonataQuery(final String query, final String json) {
 		if (query == null) {
 			log.error("Config-string of check was null.");
 			return null;
